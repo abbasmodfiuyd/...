ddaa0a695641c39c2b9bb128cd0fd0e5a151af68
abbasmodfiuyd <154954628+abbasmodfiuyd@users.noreply.github.com>
2026-03-08 08:14:40 +0000

    Initial commit: Basic ToDo app with CRUD

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ...
+# ToDo SaaS
+
+A simple ToDo application built with Flask and SQLAlchemy.
+
+## Setup
+
+1. Install dependencies: `pip install -r requirements.txt`
+2. Run migrations: `flask db upgrade`
+3. Run the app: `python app.py`
+
+## Docker
+
+Build: `docker build -t todo-saas .`
+Run: `docker run -p 5000:5000 todo-saas`
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Task
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    title = request.form.get('title')
+    if title:
+        task = Task(title=title)
+        db.session.add(task)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
